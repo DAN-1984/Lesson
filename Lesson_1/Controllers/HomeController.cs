8e847662847c352aa22f238bbe64afa22224dc7f
Данир Дашкин <userr1819@yandex.ru>
2020-03-04 17:50:45 +0500

    Карточка сотрудника

--- Lesson_1/Controllers/HomeController.cs
+++ Lesson_1/Controllers/HomeController.cs
@@ -2,12 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lesson_1.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lesson_1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<Employee> _employees = new List<Employee>()
+        {
+            new Employee()
+            {
+                Id = 1,
+                FirstName = "Иванов",
+                SurName = "Иван",
+                Patronymic = "Иванович",
+                Age = 38
+            },
+            new Employee()
+            {
+                Id = 2,
+                FirstName = "Петров",
+                SurName = "Петр",
+                Patronymic = "Петрович",
+                Age = 35
+            },
+            new Employee()
+            {
+                Id = 3,
+                FirstName = "Сидоров",
+                SurName = "Коля",
+                Patronymic = "Александрович",
+                Age = 40
+            }
+        };
         public IActionResult Index()
         {
             return View();
@@ -15,6 +43,18 @@ namespace Lesson_1.Controllers
         public IActionResult SomeAction()
         {
             return View();
+        }
+        public IActionResult Employees()
+        {
+            return View(_employees);
+        }
+        public IActionResult Employee(int id)
+        {
+            var employee_1 = _employees.FirstOrDefault(e => e.Id == id);
+            if (employee_1 == null)
+                return NotFound();
+            else
+            return View(employee_1);
         }
     }
 }
