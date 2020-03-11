d1258534c40e6cfa87ba7d7782d4222c83ce8290
Данир Дашкин <userr1819@yandex.ru>
2020-03-11 18:57:05 +0500

    Создание нового сотрудника

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -23,9 +23,56 @@ namespace WebStore.Controllers
                 return NotFound();
             return View(employee);
         }
-     
+
+        public IActionResult Create()
+        {
+            return View(new Employee());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee Employee)
+        {
+            if (Employee is null)
+                throw new ArgumentNullException(nameof(Employee));
+            if (!ModelState.IsValid)
+                return View(Employee);
+            _EmployeesData.Add(Employee);
+            _EmployeesData.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
 
 
+        public IActionResult Edit(int? Id)
+        {
+            if (Id is null) return View(new Employee());
+
+            if (Id < 0)
+                return BadRequest();
+            var employee = _EmployeesData.GetById((int)Id);
+
+            if (employee is null)
+                return NotFound();
+            return View(employee);
+        }
+        [HttpPost]
+        public IActionResult Edit(Employee Employee)
+        {
+            if (Employee is null)
+                throw new ArgumentNullException();
+            if (!ModelState.IsValid)
+                return View(Employee);
+
+            var id = Employee.Id;
+            if (id == 0)
+                _EmployeesData.Add(Employee);
+            else
+                _EmployeesData.Edit(id, Employee);
+
+            _EmployeesData.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
 
 
         //public IActionResult Edit1(int Id)
