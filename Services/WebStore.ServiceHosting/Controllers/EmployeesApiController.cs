19b9c6d9c509c96c25e2f35a5a561c2c7a38683a
Данир Дашкин <userr1819@yandex.ru>
2020-05-16 17:39:44 +0500

    Контроллер API сотрудников

---
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Employees)]
+    [ApiController]
+    public class EmployeesApiController : ControllerBase, IEmpoyeesData
+    {
+        private readonly IEmpoyeesData _EmployeesData;
+
+        public EmployeesApiController(IEmpoyeesData EmployeesData) => _EmployeesData = EmployeesData;
+        [HttpPost]
+        public void Add(Employee Employee) => _EmployeesData.Add(Employee);
+        
+        [HttpDelete("{id}")]
+        public bool Delete(int id) => _EmployeesData.Delete(id);
+        
+        [HttpPut("{id}")]
+        public void Edit(int id, Employee Employee) => _EmployeesData.Edit(id, Employee);
+        
+        [HttpGet]
+        public IEnumerable<Employee> GetAll() => _EmployeesData.GetAll();
+        
+        [HttpGet("{id}")]
+        public Employee GetById(int id) => _EmployeesData.GetById(id);
+        
+        [NonAction]
+        public void SaveChanges() => _EmployeesData.SaveChanges();
+        
+    }
+}
