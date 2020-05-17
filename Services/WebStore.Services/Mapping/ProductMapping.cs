435ddbc6d1b1889c2c93208d92f8f5de60563f6d
Данир Дашкин <userr1819@yandex.ru>
2020-05-17 17:17:16 +0500

    Проекции данных для DTO моделей товаров, секций, брендов

--- Services/WebStore.Services/Mapping/ProductMapping.cs
+++ Services/WebStore.Services/Mapping/ProductMapping.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.DTO.Products;
 using WebStore.Domain.Entities;
 using WebStore.Models;
+using WebStore.Services.Mapping;
 using WebStore.ViewModels;
 
 namespace WebStore.Infrastructure.Mapping
@@ -21,5 +23,33 @@ namespace WebStore.Infrastructure.Mapping
         };
 
         public static IEnumerable<ProductViewModel> ToView(this IEnumerable<Product> p) => p.Select(ToView);
+
+        public static ProductDTO ToDTO(this Product Product) => Product is null ? null : new ProductDTO
+        {
+            Id = Product.Id,
+            Name = Product.Name,
+            Order = Product.Order,
+            Price = Product.Price,
+            ImageUrl = Product.ImageUrl,
+            Brand = Product.Brand.ToDTO(),
+            Section = Product.Section.ToDTO(),
+        };
+
+        public static Product FromDTO(this ProductDTO Product) => Product is null ? null : new Product
+        {
+            Id = Product.Id,
+            Name = Product.Name,
+            Order = Product.Order,
+            Price = Product.Price,
+            ImageUrl = Product.ImageUrl,
+            BrandId = Product.Brand?.Id,
+            Brand = Product.Brand.FromDTO(),
+            SectionId = Product.Section.Id,
+            Section = Product.Section.FromDTO(),
+        };
+
+        public static IEnumerable<Product> FromDTO(this IEnumerable<ProductDTO> Products) => Products?.Select(FromDTO);
+
+        public static IEnumerable<ProductDTO> ToDTO(this IEnumerable<Product> Products) => Products?.Select(ToDTO);
     }
 }
