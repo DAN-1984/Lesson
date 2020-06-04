425de1cebfde27598976e9652e9b3f92c937b257
Данир Дашкин <userr1819@yandex.ru>
2020-06-04 17:02:57 +0500

    Добавление постраничного разбиения каталога товаров в визуальной части

--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using WebStore.Domain.Entities;
+using WebStore.Domain.ViewModels;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Infrastructure.Mapping;
 using WebStore.ViewModels;
@@ -12,21 +14,27 @@ namespace WebStore.Controllers
     {
         private readonly IProductData _ProductData;
         private readonly ILogger<CatalogController> _Logger;
-        public CatalogController(IProductData ProductData, ILogger<CatalogController> Logger) 
+        private readonly IConfiguration _Configuration;
+        public CatalogController(IProductData ProductData, ILogger<CatalogController> Logger, IConfiguration Configuration) 
         {
             _Logger = Logger;
-            _ProductData = ProductData; 
+            _ProductData = ProductData;
+            _Configuration = Configuration;
         }
 
-        public IActionResult Shop(int? SectionId, int? BrandId)
+        public IActionResult Shop(int? SectionId, int? BrandId, int Page = 1)
         {
+            var page_size = int.TryParse(_Configuration["PageSize"], out var size) ? size : (int?)null;
+
             _Logger.LogInformation("Запрошен каталог товаров для секции:{0} и бренда:{1}",
                 SectionId?.ToString() ?? "--", BrandId?.ToString() ?? "--");
 
             var filter = new ProductFilter
             {
                 SectionId = SectionId,
-                BrandId = BrandId
+                BrandId = BrandId,
+                Page = Page,
+                PageSize = page_size
             };
             var products = _ProductData.GetProducts(filter);
 
@@ -34,7 +42,13 @@ namespace WebStore.Controllers
             {
                 SectionId = SectionId,
                 BrandId = BrandId,
-                Products = products.Products.FromDTO().Select(ProductMapping.ToView).OrderBy(p => p.Order)
+                Products = products.Products.FromDTO().Select(ProductMapping.ToView).OrderBy(p => p.Order),
+                PageViewModel = new PageViewModel
+                {
+                    PageSize = page_size ?? 0,
+                    PageNumber = Page,
+                    TotalItems = products.TotalCount
+                }
             });
         }
 
